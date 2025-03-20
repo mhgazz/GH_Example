1b66d26e729978b37dc8dacffd0715515d2bde4d
Ricardo Núñez Valencia <ricardo141516@hotmail.com>
2025-03-20 15:02:21 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 def test_hello_world():
     from app import app
     with app.test_client() as c:
